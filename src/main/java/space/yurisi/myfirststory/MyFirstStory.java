10c9e662ca9beb47d6162a24ea916a6ba5e0a1dc
ch4rindo <96crk.nk@gmail.com>
2023-10-01 03:55:05 +0900

    頑張った

--- src/main/java/space/yurisi/myfirststory/MyFirstStory.java
+++ src/main/java/space/yurisi/myfirststory/MyFirstStory.java
@@ -1,17 +1,31 @@
 package space.yurisi.myfirststory;
 
+import net.kyori.adventure.text.Component;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class MyFirstStory extends JavaPlugin {
+public final class MyFirstStory extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        getServer().getPluginManager().registerEvents(this, this);
 
+        getLogger().info("プラグインを読み込んだで");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        String name = player.getName();
+
+        event.joinMessage(Component.text(name + "が参加したわよ"));
     }
 }
